792f3a079e18061f0d1843bce8b53f7bedd802cb
cliffkur <kurniawancliff@gmail.com>
2018-12-11 00:51:49 +1100

    added html code in send_form_email.php
    so not just a bunch of texts

--- Design 2/send_form_email.php
+++ Design 2/send_form_email.php
@@ -3,7 +3,7 @@ if(isset($_POST['submit'])) {
  
     // EDIT THE 2 LINES BELOW AS REQUIRED
     $email_to = "kurniawancliff@gmail.com";
-    $email_subject = "New Client from ".$email_from;
+    $email_subject = "New Client";
  
     function died($error) {
         // your error code can go here
@@ -82,7 +82,124 @@ $headers = 'From: '.$email_from."\r\n".
  
 <!-- include your own success html here -->
  
-Thank you for contacting me. I will be in touch with you very soon. click <a href="index.html">here</a> to go back to the main page.
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- 
+    More Templates Visit ==> ProBootstrap.com
+    Free Template by ProBootstrap.com under the License Creative Commons 3.0 ==> (probootstrap.com/license)
+
+    IMPORTANT: You can do whatever you want with this template but you need to keep the footer link back to ProBootstrap.com
+    -->
+    <title>Cliff Kurniawan Photography</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Free HTML5 Website Template by ProBootstrap.com" />
+    <meta name="keywords" content="free bootstrap 4, free bootstrap 4 template, free website templates, free html5, free template, free website template, html5, css3, mobile first, responsive" />
+    <meta name="author" content="ProBootstrap.com" />
+    
+    <!-- <link href="https://fonts.googleapis.com/css?family=Work+Sans" rel="stylesheet"> -->
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/open-iconic-bootstrap.min.css">
+    
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+
+    <link rel="stylesheet" href="css/icomoon.css">
+    <link rel="stylesheet" href="css/animate.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+    <link rel="icon" href="favicon.ico" type="image/x-icon" />
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+  </head>
+  <body>
+    
+    <aside class="probootstrap-aside js-probootstrap-aside">
+      <a href="#" class="probootstrap-close-menu js-probootstrap-close-menu d-md-none"><span class="oi oi-arrow-left"></span> Close</a>
+      <div class="probootstrap-site-logo probootstrap-animate" data-animate-effect="fadeInLeft">
+        
+        <a href="index.html" class="mb-2 d-block probootstrap-logo">Cliff Kurniawan</a>
+      </div>
+      <div class="probootstrap-overflow">
+        <nav class="probootstrap-nav">
+          <ul>
+            <li class="probootstrap-animate" data-animate-effect="fadeInLeft"><a href="index.html">Gallery</a></li>
+            <li class="probootstrap-animate" data-animate-effect="fadeInLeft"><a href="about.html">About</a></li>
+            <li class="probootstrap-animate active" data-animate-effect="fadeInLeft"><a href="contact.html">Contact</a></li>
+          </ul>
+        </nav>
+        <footer class="probootstrap-aside-footer probootstrap-animate" data-animate-effect="fadeInLeft">
+          <ul class="list-unstyled d-flex probootstrap-aside-social">
+            <li><a href="https://www.facebook.com/CliffKur" class="p-2"><span class="icon-facebook"></span></a></li>
+            <li><a href="https://www.instagram.com/cliffkur" class="p-2"><span class="icon-instagram"></span></a></li>
+          </ul>
+          <p>&copy; 2018 <a href="index.html" target="_blank">Cliff Kurniawan</a>.<br>Aside Theme by <a href="https://probootstrap.com/" target="_blank">ProBootstrap.com</a></p>
+        </footer>
+      </div>
+    </aside>
+
+
+    <main role="main" class="probootstrap-main js-probootstrap-main">
+      <div class="probootstrap-bar">
+        <a href="#" class="probootstrap-toggle js-probootstrap-toggle"><span class="oi oi-menu"></span></a>
+        <div class="probootstrap-main-site-logo"><a href="index.html">Cliff Kurniawan</a></div>
+      </div>
+      <div class="container-fluid">
+        <div class="row justify-content-center">
+          <div class="col-xl-8 col-lg-12">
+            <p class="mb-5"><img src="images/LawFrieda6.jpeg" alt="Free Bootstrap 4 Template by ProBootstrap.com" class="img-fluid"></p>
+
+            <div class="row">
+              <div class="col-xl-8 col-lg-12 mx-auto">
+                <h1 class="mb-3">Thank You!</h1>
+                  <p>Thank you for contacting me. I will be in touch with you as soon as possible.</p>
+                  
+
+                
+               
+              </div>
+            </div>
+            
+          </div>
+        </div>
+        <!-- END row -->
+
+        
+
+            
+         
+
+      </div>
+
+      <div class="container-fluid d-md-none">
+        <div class="row">
+          <div class="col-md-12">
+            <ul class="list-unstyled d-flex probootstrap-aside-social">
+            <li><a href="https://www.facebook.com/CliffKur" class="p-2"><span class="icon-facebook"></span></a></li>
+            <li><a href="https://www.instagram.com/cliffkur" class="p-2"><span class="icon-instagram"></span></a></li>
+          </ul>
+            <p>&copy; 2018 <a href="index.html" target="_blank">Cliff Kurniawan</a>.<br>Aside Theme by <a href="https://probootstrap.com/" target="_blank">ProBootstrap.com</a></p>
+          </div>
+        </div>
+      </div>
+
+    </main>
+
+    
+
+    <script src="js/jquery-3.2.1.slim.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/jquery.waypoints.min.js"></script>
+    <script src="js/imagesloaded.pkgd.min.js"></script>
+
+    <script src="js/main.js"></script>
+
+    
+  </body>
+</html>
  
 <?php
  
